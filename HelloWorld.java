d33550cad655aae82151b251c2ae7f48559795b0
akiulacre <108714567+akiulacre@users.noreply.github.com>
2022-07-05 14:44:33 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,11 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		int i;
+
+		for (i = 0; i < 11; i++)
+		{
+			System.out.println(i);
+		}
 	}
 }
